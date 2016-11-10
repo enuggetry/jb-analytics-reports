8b076badae9f43d39570a8ddc9f2a2d1693cce39
enuggetry <github@meyao.net>
2016-11-10 08:28:45 -0800

    add a few comments

--- db.sample.php
+++ db.sample.php
@@ -1,5 +1,8 @@
 <?php
 
+/*
+ * this the basic template for db.php and db_wordpress.php
+ */
 $dbh = mysql_connect('localhost','username','password');
 if (!$dbh) {
   die('Could not connect: ' . mysql_error());
