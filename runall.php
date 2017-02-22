8a741f27c513e9886754f0abbd7732ef0d3bbf2c
enuggetry <github@meyao.net>
2017-02-22 15:51:53 -0800

    add new reports. remove obsolete.

--- runall.php
+++ runall.php
@@ -1,20 +1,21 @@
 <?php
 
-
-run_exec('php accesses-by-host-12mon.php');         
+// by month/quarter
+run_exec('php report-by-QorM.php MONTH');         
+run_exec('php report-by-QorM.php QUARTER');         
 run_exec('php downloads-by-month.php');
-run_exec('php accesses-by-host-all.php');
-run_exec('php get-last-few.php');
-run_exec('php accesses-by-month-apollo.php');
-run_exec('php mean-tracks-12-mon.php');
-run_exec('php accesses-by-month.php');
+
+// last 12 months 
+run_exec('php accesses-by-host-12mon.php');         
 run_exec('php top-hosts-last-12-mon.php');
 run_exec('php unique-clients-12-mon.php');
-run_exec('php active-hosts-by-month.php');
 run_exec('php trackcount-by-host-12mo.php');
 run_exec('php trackcount-counts-12-mon.php');
-run_exec('php mean-tracks-by-mo.php');
+run_exec('php mean-tracks-12-mon.php');
 run_exec('php plugins-by-host-12mo.php');
+
+// other
+run_exec('php accesses-by-host-all.php');
 
 function run_exec($cmd) {
     
